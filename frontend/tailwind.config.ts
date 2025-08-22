971d22487eb810435db0b85d7454a6785e9fff77
contactevin2u <contact@evin2u.com>
2025-08-22 17:38:26 +0800

    fix: update tailwind postcss plugin

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './styles/**/*.css'],
   theme: {
     extend: {
       colors: {
