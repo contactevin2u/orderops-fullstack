ee791dfbf1644ec9605b2e59f852a44dc5fd02e6
contactevin2u <contact@evin2u.com>
2025-08-25 23:05:59 +0800

    fix: correct nextauth config

---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+};
+
+export default NextAuth(authOptions);
+
