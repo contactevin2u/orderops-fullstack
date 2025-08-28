91c25f09cabfc1a5e4bf3b2455f2c7638c0369e9
contactevin2u <contact@evin2u.com>
2025-08-29 01:34:32 +0800

    Delete driver-app directory

--- driver-app/android/app/src/main/java/com/yourco/driverAA/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.yourco.driverAA
-
-import com.facebook.react.ReactActivity
-
-class MainActivity : ReactActivity() {
-  // Matches the name registered via registerRootComponent("main")
-  override fun getMainComponentName(): String = "main"
-}
