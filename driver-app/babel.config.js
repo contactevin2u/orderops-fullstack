2d6600568d4228c637df834a4fdf8bbecdf457e1
contactevin2u <contact@evin2u.com>
2025-08-29 17:32:30 +0800

    chore(driver-app): update notifee and android build workflow

--- driver-app/babel.config.js
+++ driver-app/babel.config.js
@@ -3,10 +3,6 @@ module.exports = function (api) {
   const plugins = [
     ['module-resolver', { root: ['./'], alias: { '@': './' } }],
   ];
-  // Include Reanimated plugin only if installed; keep it LAST.
-  try {
-    require.resolve('react-native-reanimated/package.json');
-    plugins.push('react-native-reanimated/plugin');
-  } catch {}
+  try { require.resolve('react-native-reanimated/package.json'); plugins.push('react-native-reanimated/plugin'); } catch {}
   return { presets: ['babel-preset-expo'], plugins };
 };
