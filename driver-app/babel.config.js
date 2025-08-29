5ba408bbf0ec1597fa44361c1e3870b2389997cb
contactevin2u <contact@evin2u.com>
2025-08-29 12:12:25 +0800

    chore: fix babel config and improve release diagnostics

--- driver-app/babel.config.js
+++ driver-app/babel.config.js
@@ -1,26 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@core": "./src/core",
-            "@infra": "./src/infrastructure",
-            "@presentation": "./src/presentation",
-            "@shared": "./src/shared",
-          },
-        },
-      ],
-      [
-        "@babel/plugin-transform-inline-environment-variables",
-        {
-          include: ["API_BASE", "FIREBASE_PROJECT_ID"],
-        },
-      ],
+      'expo-router/babel',
+      // NOTE: This plugin must be last
+      'react-native-reanimated/plugin',
     ],
   };
 };
