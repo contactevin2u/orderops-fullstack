3d7d3e0c8e3b3ed9b1944b2a6661eeebfc27ceac
contactevin2u <contact@evin2u.com>
2025-08-30 00:12:36 +0800

    chore: configure android release build

--- driver-app/babel.config.js
+++ driver-app/babel.config.js
@@ -1,18 +1,11 @@
+// driver-app/babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['module-resolver', {
-        root: ['./'],
-        alias: {
-          '@': './src',
-        },
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-      }],
-      // MUST BE LAST
+      ['module-resolver', { alias: { '@': './src' } }],
       'react-native-reanimated/plugin',
     ],
   };
 };
-
