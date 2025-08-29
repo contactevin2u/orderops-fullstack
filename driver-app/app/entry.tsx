a3ccb36ebb74df9b4609c32b3ccdeb1396c69ebb
contactevin2u <contact@evin2u.com>
2025-08-29 12:03:46 +0800

    ci: build driver app and register background handler

---
+++ driver-app/app/entry.tsx
@@ -0,0 +1,5 @@
+// Ensure background FCM handler is registered for Android headless tasks
+import "../src/infrastructure/firebase/background-handler";
+
+export { default } from "expo-router/entry";
+
