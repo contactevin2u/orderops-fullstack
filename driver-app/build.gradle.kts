6a4eabb07b967788109a557e80c28cefd9365f18
contactevin2u <contact@evin2u.com>
2025-08-30 10:56:28 +0800

    Update build.gradle.kts

--- driver-app/build.gradle.kts
+++ driver-app/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.25" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.firebase.appdistribution") version "4.0.1" apply false
 }
