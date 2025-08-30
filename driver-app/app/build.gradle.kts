be17dfa036ba961c4306d9bd472ca5f5f07975af
contactevin2u <contact@evin2u.com>
2025-08-30 09:30:29 +0800

    Update build.gradle.kts

--- driver-app/app/build.gradle.kts
+++ driver-app/app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
+    kotlin("plugin.serialization") version "1.9.25"
 }
 
 android {
